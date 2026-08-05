f20427257f859cfe20d24fe98c82a0610fcf190e
Ploy Builder <builder@runploy.com>
2026-08-05 13:06:37 -0700

    auto: src/components/pages/laterpay.tsx, src/components/pages/slalome.tsx, src/components/pages/truvalue-wealth.tsx, src/components/pages/wickr.tsx

---
+++ src/components/pages/laterpay.tsx
@@ -0,0 +1,5 @@
+import ProjectCaseStudyTemplate from "./project-case-study";
+import { PORTFOLIO_CASE_STUDIES } from "./portfolio-case-study-content";
+
+/** @ployComponent @ployComponentId laterpay-case-study-page @ployComponentType page @ployComponentPattern case-study @ployComponentDescription LaterPay portfolio case study using the shared editorial project template. @ployComponentTags case-study laterpay mobile creator payments @ployComponentStatus stable */
+export default function LaterPayPage() { return <ProjectCaseStudyTemplate content={PORTFOLIO_CASE_STUDIES.laterpay} />; }
---
+++ src/components/pages/slalome.tsx
@@ -0,0 +1,5 @@
+import ProjectCaseStudyTemplate from "./project-case-study";
+import { PORTFOLIO_CASE_STUDIES } from "./portfolio-case-study-content";
+
+/** @ployComponent @ployComponentId slalome-case-study-page @ployComponentType page @ployComponentPattern case-study @ployComponentDescription Slalome portfolio case study using the shared editorial project template. @ployComponentTags case-study slalome mobile collaboration @ployComponentStatus stable */
+export default function SlalomePage() { return <ProjectCaseStudyTemplate content={PORTFOLIO_CASE_STUDIES.slalome} />; }
