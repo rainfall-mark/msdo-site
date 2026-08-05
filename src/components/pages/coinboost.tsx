90c1dccc8aeccd9b6b8825224f5eb6ca6050e2fd
Ploy Builder <builder@runploy.com>
2026-08-05 13:13:02 -0700

    auto: src/components/pages/coinboost.tsx, src/components/pages/truvalue-platform.tsx, src/components/pages/truvalue-wealth.tsx, src/components/pages/truvalue-website.tsx

--- src/components/pages/coinboost.tsx
+++ src/components/pages/coinboost.tsx
@@ -1,5 +1,13 @@
 import ProjectCaseStudyTemplate from "./project-case-study";
 import { PORTFOLIO_CASE_STUDIES } from "./portfolio-case-study-content";
 
-/** @ployComponent @ployComponentId coinboost-case-study-page @ployComponentType page @ployComponentPattern case-study @ployComponentDescription Coinboost portfolio case study using the shared editorial project template. @ployComponentTags case-study coinboost mobile crypto @ployComponentStatus stable */
+/**
+ * @ployComponent
+ * @ployComponentId coinboost-case-study-page
+ * @ployComponentType page
+ * @ployComponentPattern case-study
+ * @ployComponentDescription Coinboost portfolio case study using the shared editorial project template.
+ * @ployComponentTags case-study coinboost mobile crypto
+ * @ployComponentStatus stable
+ */
 export default function CoinboostPage() { return <ProjectCaseStudyTemplate content={PORTFOLIO_CASE_STUDIES.coinboost} />; }
--- src/components/pages/truvalue-wealth.tsx
+++ src/components/pages/truvalue-wealth.tsx
@@ -1,5 +1,13 @@
 import ProjectCaseStudyTemplate from "./project-case-study";
 import { PORTFOLIO_CASE_STUDIES } from "./portfolio-case-study-content";
 
-/** @ployComponent @ployComponentId truvalue-wealth-case-study-page @ployComponentType page @ployComponentPattern case-study @ployComponentDescription Truvalue Wealth portfolio case study using the shared editorial project template. @ployComponentTags case-study truvalue wealth esg tablet @ployComponentStatus stable */
+/**
+ * @ployComponent
+ * @ployComponentId truvalue-wealth-case-study-page
+ * @ployComponentType page
+ * @ployComponentPattern case-study
+ * @ployComponentDescription Truvalue Wealth portfolio case study using the shared editorial project template.
+ * @ployComponentTags case-study truvalue wealth esg tablet
+ * @ployComponentStatus stable
+ */
 export default function TruvalueWealthPage() { return <ProjectCaseStudyTemplate content={PORTFOLIO_CASE_STUDIES.truvalueWealth} />; }
